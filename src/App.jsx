514b3011a066446c19f16db625290f78fb979d1e
brianmujjuni <brianmujjuni@engineer.com>
2024-05-13 21:52:54 +0300

    create dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Input from "./ui/Input";
-import Button from "./ui/Button";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 export default function App() {
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Heading as="h1">Automex</Heading>
-          <div>
-            <Button variation="primary" size="medium">
-              Sex
-            </Button>
-            <Button variation="secondary" size="medium">
-              Condoms
-            </Button>
-            <Input type="tel"></Input>
-          </div>
-        </Row>
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
