69004724febe5850092987304fd6447e82af6262
brianmujjuni <brianmujjuni@engineer.com>
2024-05-13 20:31:32 +0300

    create styled button and input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Input from "./ui/Input";
+import Button from "./ui/Button";
+
+const StyledApp = styled.div`
+  background-color: yellow;
+  padding: 20px;
+`;
 export default function App() {
-  return <div>Automex</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Button>Sex</Button>
+        <Button>Condoms</Button>
+        <Input type="tel"></Input>
+      </StyledApp>
+    </>
+  );
 }
