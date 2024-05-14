bd9ad3638385792fa9ba767c691e0052592d7653
brianmujjuni <brianmujjuni@engineer.com>
2024-05-14 21:03:36 +0300

    move images to pulic folder

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
@@ -7,5 +8,10 @@ const StyledSidebar = styled.aside`
   grid-row: 1/-1;
 `;
 export default function SideBar() {
-  return <StyledSidebar>Sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
