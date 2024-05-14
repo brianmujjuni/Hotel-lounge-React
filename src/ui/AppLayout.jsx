71db3950575018c6459ae6c3a695513d52a66f61
brianmujjuni <brianmujjuni@engineer.com>
2024-05-14 20:49:28 +0300

    create app layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,3 +1,27 @@
+import { Outlet } from "react-router-dom";
+import SideBar from "./Sidebar";
+import Header from "./Header";
+import styled from "styled-components";
+
+const Main = styled.main`
+  padding: 4rem 4.8rem 6.4rem;
+  background-color: var(--color-grey-50);
+`;
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
 export default function AppLayout() {
-  return <div>App Layout</div>;
+  return (
+    <StyledAppLayout>
+      <Header />
+      <SideBar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
 }
