bb9d0dd8b2cabe0ec7f4632743864b824967be79
brianmujjuni <brianmujjuni@engineer.com>
2024-05-20 18:42:31 +0300

    remove useEffect in cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -4,10 +4,6 @@ import Row from "../ui/Row";
 import { getCabines } from "../services/apiCabines";
 
 function Cabins() {
-  useEffect(() => {
-    getCabines().then((data) => console.log(data));
-  }, []);
-
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
