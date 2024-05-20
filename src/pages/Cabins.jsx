753ed6c8e10eae4066a78e952ad088b09c97b46c
brianmujjuni <brianmujjuni@engineer.com>
2024-05-20 18:40:31 +0300

    add react query

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabines } from "../services/apiCabines";
 
 function Cabins() {
+  useEffect(() => {
+    getCabines().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
